8adabde509c1723967b41c9c0b41169b326104c7
Damian Soriano <damiansoriano@gmail.com>
2015-07-14 17:28:12 +0100

    Use kingpin to pass arguments

--- p2.go
+++ p2.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+    "fmt"
     "os"
     "strings"
     "text/template"
+    "gopkg.in/alecthomas/kingpin.v2"
 )
 
 func getContext() map[string]string{
@@ -16,7 +18,15 @@ func getContext() map[string]string{
     return ctx
 }
 
+
+var (
+    templatePath = kingpin.Arg("template", "Template.").Required().String()
+)
+
 func main() {
+    kingpin.Parse()
+    fmt.Printf("Template name is %s", *templatePath)
+
     t,err := template.New("conf").Parse("{{.LANGUAGE}}")
     if err != nil { panic(err) }
     m := getContext()
